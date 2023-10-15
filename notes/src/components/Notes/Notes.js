65141cbe5054d95ca41f3e95a35cc6c43f5f8e01
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-10-15 15:56:01 +0530

    updated

--- notes/src/components/Notes/Notes.js
+++ notes/src/components/Notes/Notes.js
@@ -7,7 +7,7 @@ import Note from "../Note/Note";
 import "./Notes.scss";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = process.env.BASE_URL || "http://localhost:3001";
+axios.defaults.baseURL = "http://keepnotes.ap-south-1.elasticbeanstalk.com";
 
 const Notes = () => {
   const [addNote, setAddNote] = useState({
