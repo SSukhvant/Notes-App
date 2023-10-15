fbdfd1a54c7f454031c2b030e74468407e8b7289
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-10-15 15:50:19 +0530

    updated

--- notes/src/components/Notes/Notes.js
+++ notes/src/components/Notes/Notes.js
@@ -6,6 +6,7 @@ import Modal from "../Modal/Modal";
 import Note from "../Note/Note";
 import "./Notes.scss";
 
+axios.defaults.withCredentials = true;
 axios.defaults.baseURL = process.env.BASE_URL || "http://localhost:3001";
 
 const Notes = () => {
